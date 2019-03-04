9d2ecb91ba5b53465bbf8c91204d78bc6dacb221
piotrbartnik <piotr.bartnik@hotmail.com>
2019-03-05 00:33:38 +0100

    configured gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ let babel = require('gulp-babel');
 let uglify = require('gulp-uglify');
 let pump = require('pump');
 const image = require('gulp-image');
+let sass = require('gulp-sass');
+let uglifycss = require('gulp-uglifycss');
+
+sass.compiler = require('node-sass');
 
 
 gulp.task('babel', () =>
@@ -25,6 +29,25 @@ gulp.task('compress', function (cb) {
   );
 });
 
+gulp.task('sass',  () => {
+  return gulp.src('src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('css', function () {
+  gulp.src('src/css/*.css')
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('dist/css'));
+    return new Promise(function (resolve, reject) {
+      console.log("Finished uglifycss");
+      resolve();
+    });
+});
+
 gulp.task('image', function () {
   gulp.src('src/img/*')
     .pipe(image())
@@ -40,9 +63,11 @@ gulp.task('watch', () => {
   gulp.watch('src/img/*', gulp.series('image'));
   gulp.watch('src/js/*.js', gulp.series('babel'));
   gulp.watch('src/js/js_babel/*.js', gulp.series('compress'));
+  gulp.watch('src/sass/*.scss', gulp.series('sass'));
+  gulp.watch('src/css/*.css', gulp.series('css'));
 });
 
 
 
 
-gulp.task('default', gulp.series('babel', 'compress', 'image', 'watch'));
+gulp.task('default', gulp.series('babel', 'compress', 'image', 'sass', 'css', 'watch'));
