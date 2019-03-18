b8733848f785880680be7aae7960ebe7e17b5767
piotrbartnik <piotr.bartnik@hotmail.com>
2019-03-18 23:56:05 +0100

    added some tests

--- src/unit_tests/function.test.js
+++ src/unit_tests/function.test.js
@@ -1,16 +1,22 @@
-var mathModule  = require('../js/index');
+var mathModule = require('../js/index');
 
 let splitFunc = (str) => {
- return str.split('')
+  return str.split('')
 }
 
 
-test('adding', () => {
-  expect(mathModule.calculateResult(['1','+', '2'])).toBe(3);
+test('adding substracting', () => {
+  expect(mathModule.calculateResult(splitFunc('1+2'))).toBe(3);
+  expect(mathModule.calculateResult(splitFunc('14+3'))).toBe(17);
+  expect(mathModule.calculateResult(splitFunc('8-82'))).toBe(-74);
+  expect(mathModule.calculateResult(splitFunc('1+222'))).toBe(223);
 });
 
-test('multiplying', () => {
-  expect(mathModule.calculateResult(['2','*', '3'])).toBe(6);
+test('multiplying dividing', () => {
+  expect(mathModule.calculateResult(splitFunc('1*2'))).toBe(2);
+  expect(mathModule.calculateResult(splitFunc('14*14'))).toBe(196);
+  expect(mathModule.calculateResult(splitFunc('12/3'))).toBe(4);
+  expect(mathModule.calculateResult(splitFunc('222/222'))).toBe(1);
 });
 
 test('misc', () => {
