8860a14fab8ab2d39d8d996c9037db9a9112ae1d
piotrbartnik <piotr.bartnik@hotmail.com>
2019-03-31 22:42:55 +0200

    added rpn js

---
+++ src/js/js_babel/rpn.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var rpn = function rpn(eq) {
+  var operands = ["+", "-", "*", "/"];
+  var nums = [];
+  var arrOperands = [];
+
+  for (var i = 0; i < eq.length; i++) {
+    if (/[0-9]+/.test(eq[i])) {
+      nums.push(eq[i]);
+    }
+
+    if (operands.indexOf(eq[i]) != -1) {
+      if (eq[i] === "-" && arrOperands[0] === "*") {
+        nums.push(arrOperands[0]);
+        arrOperands.splice(0, 1);
+      }
+
+      arrOperands.unshift(eq[i]);
+    }
+  }
+
+  return nums.concat(arrOperands).join(" ");
+};
+
+rpn(['1', '+', '2', '-', '6']);
