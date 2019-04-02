3e74ae840603a47b5fcd68db4e1900786a2dcaf3
piotrbartnik <piotr.bartnik@hotmail.com>
2019-04-02 23:14:40 +0200

    added js test

--- src/unit_tests/function.test.js
+++ src/unit_tests/function.test.js
@@ -6,23 +6,30 @@ let splitFunc = (str) => {
 }
 
 test('adding substracting', () => {
-  expect(mathModule.calculateResult(splitFunc('1+2'))).toBe(3);
-  expect(mathModule.calculateResult(splitFunc('14+3'))).toBe(17);
-  expect(mathModule.calculateResult(splitFunc('8-82'))).toBe(-74);
-  expect(mathModule.calculateResult(splitFunc('1+222'))).toBe(223);
+  expect(mathModule.calculateResult(splitFunc('1+2'))).toEqual([3]);
+  expect(mathModule.calculateResult(splitFunc('14+3'))).toEqual([17]);
+  expect(mathModule.calculateResult(splitFunc('8-82'))).toEqual([-74]);
+  expect(mathModule.calculateResult(splitFunc('1+222'))).toEqual([223]);
 });
 
 test('multiplying dividing', () => {
-  expect(mathModule.calculateResult(splitFunc('1*2'))).toBe(2);
-  expect(mathModule.calculateResult(splitFunc('14*14'))).toBe(196);
-  expect(mathModule.calculateResult(splitFunc('12/3'))).toBe(4);
-  expect(mathModule.calculateResult(splitFunc('222/222'))).toBe(1);
+  expect(mathModule.calculateResult(splitFunc('1*2'))).toEqual([2]);
+  expect(mathModule.calculateResult(splitFunc('14*14'))).toEqual([196]);
+  expect(mathModule.calculateResult(splitFunc('12/3'))).toEqual([4]);
+  expect(mathModule.calculateResult(splitFunc('222/222'))).toEqual([1]);
 });
 
 test('misc', () => {
-  expect(mathModule.calculateResult(splitFunc('1+2+15-6*27'))).toBe(-144);
+  expect(mathModule.calculateResult(splitFunc('1+2+15-6*27'))).toEqual([-144]);
 });
 
 test('rpn', () => {
   expect(rpnModule.rpn('1+2')).toBe('1 2 +');
+  expect(rpnModule.rpn('1*2')).toBe('1 2 *');
+  expect(rpnModule.rpn('1+2-5')).toBe('1 2 5 - +');
+  expect(rpnModule.rpn('1+2*4')).toBe('1 2 4 * +');
+  expect(rpnModule.rpn('1*2+10')).toBe('1 2 * 10 +');
+  expect(rpnModule.rpn('1/2')).toBe('1 2 /');
+  expect(rpnModule.rpn('1+2/10')).toBe('1 2 10 / +');
+  expect(rpnModule.rpn('1/2*5')).toBe('1 2 / 5 *');
 });
